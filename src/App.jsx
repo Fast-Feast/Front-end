9de733e8c3449e30a978085af692621a356f88b9
Joao-Manuel-S-M <jmsmarques8213@gmail.com>
2023-06-03 15:46:14 -0300

    feat: Crate new feature Carousel-Banners

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Carousel from './Components/Carousel/Carousel'
+import CarouselBanners from './Components/CarouselBanners/CarouselBanners'
 
 function App() {
 
@@ -7,8 +8,8 @@ function App() {
   return (
 
     <div>
+      <CarouselBanners />
       <Carousel/>
-
     </div>
 
   )
---
+++ src/Components/CarouselBanners/CarouselBanners.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from "swiper";
+import * as S from './style';
+
+function CarouselBanner() {
+  return (
+    <S.SwiperStyled
+      spaceBetween={5}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      navigation={true}
+      modules={[Navigation, Pagination]}
+      isInfinite={true}
+      loop={true}
+    >
+      <SwiperSlide><div className="content"></div></SwiperSlide>
+      <SwiperSlide><div className="content"></div></SwiperSlide>
+      <SwiperSlide><div className="content"></div></SwiperSlide>
+      <SwiperSlide><div className="content"></div></SwiperSlide>
+    </S.SwiperStyled>
+  );
+}
+
+export default CarouselBanner
