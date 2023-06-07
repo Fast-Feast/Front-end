5ffbd94113a9b74b12d946474c830dd7ba33c036
Joao-Manuel-S-M <jmsmarques8213@gmail.com>
2023-06-07 17:37:08 -0300

    Feat: Create new component footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Carousel from './Components/Carousel/Carousel'
 import CarouselBanners from './Components/CarouselBanners/CarouselBanners'
 import CarouselOfertas from './Components/CarouselOfertas/CarouselOfertas'
+import Footer from './Components/Footer/Footer'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Carousel/>
       <CarouselBanners />
       <CarouselOfertas />
+      <Footer />
     </div>
 
   )
